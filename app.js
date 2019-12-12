40ec5512d7944b226bf6bbed0aded3643fed0801
FilipeMts <jfgmatos@outlook.com>
2019-12-12 15:12:15 +0000

    update

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const bindUserToViewLocals = require('./middleware/bind-user-to-view-locals.js')
 const passportConfigure = require('./passport-configuration.js');
 const indexRouter = require('./routes/index');
 const authenticationRouter = require('./routes/authentication');
+const teamsRouter = require('./routes/teams');
 
 const app = express();
 
@@ -56,6 +57,8 @@ app.use(bindUserToViewLocals);
 
 app.use('/', indexRouter);
 app.use('/authentication', authenticationRouter);
+app.use('/', teamsRouter);
+
 
 // Catch missing routes and forward to error handler
 app.use((req, res, next) => {
--- client/src/views/Navbar/Navbar.jsx
+++ client/src/views/Navbar/Navbar.jsx
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/images/mvp_logo_full.png';
-import NavigationItems from './NavigationItems/NavigationItems'
+import NavigationItems from './NavigationItems/NavigationItems';
+import { getTeam as getTeamService } from "../../services/teams"
 import './navbar.scss';
 
-const Navbar = () => {
-  return (
-    <nav>
+class Navbar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      search: ""
+    }
+    this.handleFormSubmission = this.handleFormSubmission.bind(this);
+  }
+
+  onChangeHandler = (e) => {
+    //console.log(this.state.search)
+    //console.log(e.target.value)   
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  async handleFormSubmission(e) {
+    e.preventDefault();
+    console.log('form submission')
+    const { search } = this.state;
+    try {      
+      const team = await getTeamService({ search });      
+      //this.props.history.push(`/${search}`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    return (
+      <nav>
         <Link to="/"><img src={Logo} alt="logo" /></Link>
         <NavigationItems />
-    </nav>
-  )
+        <form onSubmit={this.handleFormSubmission} >
+          <input type="search" name="search" value={this.state.search} onChange={this.onChangeHandler}></input>
+          <button>Submit</button>
+        </form>
+      </nav>
+    )
+  }
 }
+
 
 export default Navbar;
