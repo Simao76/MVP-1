7c8eea071aa769e208768d8249bd2f2f8ac8a5b5
Simao Meneses <simaorsmeneses@gmail.com>
2019-12-16 10:05:35 +0000

    update

---
+++ apiServices/load-teams.js
@@ -0,0 +1,74 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const URI = process.env.MONGODB_URI;
+const axios = require("axios");
+
+const footballService = axios.create({
+  baseURL: "https://www.thesportsdb.com/api/v1/json/1"
+});
+
+//
+
+const getTeamsInALeague = async () => {
+  try {
+    const response = await footballService.get(`/lookup_all_teams.php?id=4356`);
+    const teams = response.data.teams;
+    console.log("get all teams", response.data);
+    return response.data.teams;
+  } catch (error) {
+    throw error;
+  }
+};
+
+/* 
+const Team = require("../models/teams");
+
+const loadAllData = async () => {
+  const teams = await getTeamsInALeague();
+  
+  const formatedTeams = teams.map(item => {
+    return {
+      idTeam: item.idTeam,
+      name: item.strTeam,
+      league: item.strLeague,
+      stadium: item.strStadium,
+      description: item.strDescriptionEN,
+      badge: item.strBadge,
+      Manager: item.strManager
+    };
+  });
+  await Team.deleteMany({});
+  const teamDocuments = await Team.create(formatedTeams);
+  console.log(teamDocuments);
+};
+/* 
+(async () => {
+  try {
+    await mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    });
+    await loadAllData();
+    await mongoose.disconnect();
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  } finally {
+    process.exit(0);    
+  } 
+})(); 
+
+*/
+
+const populateDB = async () => {
+  try {
+    await getTeamsInALeague();
+    /* await loadAllData(); */
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+module.exports.populateDB = populateDB;
--- routes/teams.js
+++ routes/teams.js
@@ -2,8 +2,20 @@
 
 const { Router } = require("express");
 const router = new Router();
-const axios = require("axios");
-/* 
-POST;
- */
+
+const Team = require("./../models/teams");
+require("../apiServices/load-teams");
+
+router.get("/listteams", (req, res, next) => {
+  console.log("BE teams.js list teams");
+  Team.find()
+    .then(teams => {
+      //console.log("leagues.js", leagues)
+      res.json({ teams });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
 module.exports = router;
