680b31785e8e972715921560497605b913d39b1b
FilipeMts <jfgmatos@outlook.com>
2019-12-18 18:39:43 +0000

    update

--- client/src/services/Sports.js
+++ client/src/services/Sports.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiService = axios.create({
-  baseURL: "",
+  baseURL: "", // tirar http://localhost:3020 para funcionar no heroku
   withCredentials: true
 });
 
